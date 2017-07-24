e42fa83afd8bd6d432a354114cbc0dfd7c32cf42
JR <jr@gongevangen.dk>
2017-07-24 16:02:21 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@
         
         socket.on('connect', function() {
             console.log('Connected to server');
-
-            socket.emit('createMessage', {
-                from: 'jr',
-                text: 'Hey. This is me'
-            });
         });
 
         socket.on('disconnect', function() {
