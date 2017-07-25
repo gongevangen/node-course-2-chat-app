0b8b0421b0ba980ee4ed54611cea1ce4e98d5e8d
JR <jr@gongevangen.dk>
2017-07-25 09:55:40 +0200

    Location link to maps implemented

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage, generateLocationMessage} = require('./utils/message')
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -35,13 +37,11 @@ io.on('connection', (socket) => {
         });
         callback('This is from the server');
     });
-        
-    // socket.broadcast.emit('newMessage', {
-    //         from: message.from,
-    //         text: message.text,
-    //         createdAt: new Date().getTime()
-    //     });
-    
+
+    socket.on('createLocationMessage', (coords) =>  {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    })
+
     socket.on('disconnect', () => {
         console.log('User was diconnected');
     });
